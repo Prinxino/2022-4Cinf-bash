7d8a929a84a246c57a646a630f70e246d9b73785
Francesco Gaggiano <francesco.gaggiano@marconicloud.it>
2022-10-04 10:27:43 +0200

    new

---
+++ grande.sh
@@ -0,0 +1,15 @@
+a=$1
+b=$2
+file=$3
+
+if [ $1 -gt $2 ]
+    then
+        if [ -e $3 ]
+        then
+            cat $3
+        else
+            ls -l
+        fi
+    else
+        echo "Error"
+    fi
