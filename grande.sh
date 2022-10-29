90457fc0a513a834c67bc5e5d98afbe3a2c6def8
Prinxino <94068624+Prinxino@users.noreply.github.com>
2022-10-29 10:57:52 +0200

    Add files via upload

--- grande.sh
+++ grande.sh
@@ -5,11 +5,11 @@ file=$3
 if [ $1 -gt $2 ]
     then
         if [ -e $3 ]
-        then
-            cat $3
-        else
-            ls -l
+            then
+                cat $3
+            else
+                ls -l
         fi
     else
         echo "Error"
-    fi
+fi
